b26cb64952764fce6ae5218c5261d91efb185513
RAES9 <erivasdeveloper@gmail.com>
2020-02-26 18:07:50 -0600

    Initial Commit

---
+++ MnCTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MnCTest
+//
+//  Created by Esteban Rivas on 2/26/20.
+//  Copyright © 2020 Esteban Rivas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
